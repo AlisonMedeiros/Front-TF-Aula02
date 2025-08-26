7399c849c87a33a8b4d30410c5f6457b2dbd686b
Alison Medeiros <Alisonmedeirosprofissional@gmail.com>
2025-08-26 20:24:27 -0300

    Implementação completa do exercício: seletores CSS, responsividade e interatividade JavaScript

--- public/js/app.js
+++ public/js/app.js
@@ -5,7 +5,12 @@ liElementCollection.forEach((liElement) => {
     liElement.addEventListener("click", (event) => {
         const liElement = event.currentTarget;
 
+        // Remove a classe .selected de todos os itens
+        liElementCollection.forEach((item) => {
+            item.classList.remove("selected");
+        });
 
-        /** CODAR AQUI */
+        // Adiciona a classe .selected ao item clicado
+        liElement.classList.add("selected");
     });
 });
